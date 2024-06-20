879b5604e404f51356d332d9eb5f1568f9798b63
carlalopesj <carlaljovito@gmail.com>
2024-06-20 14:05:20 -0300

    expo go qr code
    tirando o tunel e gerando o apk android

--- src/app/cadastro/index.tsx
+++ src/app/cadastro/index.tsx
@@ -1,6 +1,5 @@
-//Index da parte FrontEnd do cadastro
 import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Modal, Text, TouchableHighlight, StatusBar, Alert } from 'react-native';
+import { View, TextInput, Modal, Text, TouchableOpacity, StatusBar, Alert } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useFonts, LilitaOne_400Regular } from '@expo-google-fonts/lilita-one';
 import MaskInput from 'react-native-mask-input';
@@ -11,7 +10,7 @@ import { styles } from './styles';
 export default function Index() {
   const router = useRouter();
 
-  //Hooks UseState
+  // Hooks UseState
   const [id, setId] = useState('');
   const [nome, setNome] = useState('');
   const [cpf, setCPF] = useState('');
@@ -19,27 +18,27 @@ export default function Index() {
   const [genero, setGenero] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
 
-  //Definindo os gêneros
+  // Definindo os gêneros
   const sexo = [
     { id: 1, texto: 'Feminino' },
     { id: 2, texto: 'Masculino' },
     { id: 3, texto: 'Voltar' },
   ];
 
-  //Função para quando o botão cadastrar for acionado
+  // Função para quando o botão cadastrar for acionado
   const handleSubmit = async () => {
-    if (!nome || !cpf || !idade || !genero) { //Caso os campos estejam vazios
+    if (!nome || !cpf || !idade || !genero) { // Caso os campos estejam vazios
       Alert.alert("Erro", "Por favor, preencha todos os campos.");
       return("index");
     }
     
     try {
-      const response = await fetch('http://192.168.0.12:3535/addpaciente', { //Fazendo a conexão com o BackEnd. *Alterar o IP de acordo com o da sua máquina
+      const response = await fetch('http://192.168.0.12:3535/addpaciente', { // Fazendo a conexão com o BackEnd. *Alterar o IP de acordo com o da sua máquina
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        //Passando os dados
+        // Passando os dados
         body: JSON.stringify({
           nome: nome,
           cpf: cpf,
@@ -57,7 +56,7 @@ export default function Index() {
         setCPF('');
         setIdade('');
         setGenero('');
-        router.replace('/'); //Volta para a página Home no index
+        router.replace('/'); // Volta para a página Home no index
       } else {
         // Se houve um erro na requisição, vamos verificar se a resposta não está vazia antes de tentar analisar como JSON
         const responseData = await response.text();
@@ -78,7 +77,7 @@ export default function Index() {
     return <View><CustomText>Carregando...</CustomText></View>;
   }
 
-  const CPF_MASK = [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/]; //Modelo para o CPF
+  const CPF_MASK = [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/]; // Modelo para o CPF
 
   // Verifica se todos os campos obrigatórios foram preenchidos
   const isFormValid = !!id && !!nome && !!cpf && !!idade && !!genero;
@@ -172,7 +171,7 @@ export default function Index() {
                     setModalVisible(!modalVisible);
                   }}
                 >
-                  <Text >{item.texto}</Text>
+                  <Text>{item.texto}</Text>
                 </TouchableOpacity>
               ))}
             </View>
