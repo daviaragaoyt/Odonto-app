cc93eb1d6291c1ebe23421d9e1bf28e00b433c0c
Davi Aragao <daviaragaodf1@gmail.com>
2024-06-26 12:13:02 -0300

    Mudança de nota para Score

--- src/app/cadastro/styles.ts
+++ src/app/cadastro/styles.ts
@@ -63,11 +63,11 @@ export const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
+    flex: 1,
     borderWidth: 1,
     borderColor: '#1CAAFA',
     borderRadius: 5,
     backgroundColor: '#fff',
-    flex: 1,
     width: 200,
     height: 40,
     fontSize: 20,
