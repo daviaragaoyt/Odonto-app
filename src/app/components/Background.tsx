e264b6664d27aa31a1bb6448f88b78fbd6af1587
DAVI <daviaragaoyt@gmail.com>
2024-09-20 12:35:27 -0300

    update

---
+++ src/app/components/Background.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { View, ImageBackground, StyleSheet, StatusBar, ActivityIndicator } from 'react-native';
+import { Asset } from 'expo-asset';
+
+const Background: React.FC = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadAssets() {
+      await Asset.loadAsync(require('../../../assets/images/body.png'));
+      setIsLoaded(true);
+    }
+
+    loadAssets();
+  }, []);
+
+  if (!isLoaded) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" color="#0668a1" />
+      </View>
+    );
+  }
+
+  return (
+    <ImageBackground source={require('../../../assets/images/body.png')} style={styles.backgroundImage}>
+      <StatusBar barStyle={'dark-content'} />
+      <View style={styles.overlay} />
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 0.5,
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 20,
+    backgroundColor: 'transparent',
+  },
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0668a1', // Cor de fundo enquanto carrega
+  },
+});
+
+export default Background;
